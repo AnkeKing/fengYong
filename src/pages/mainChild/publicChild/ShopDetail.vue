5ab3480d363f8d69752030777f530adbc030c51b
AnkeKing <2678924540@qq.com>
2019-06-07 07:01:28 +0800

    holidly morning .play up.

---
+++ src/pages/mainChild/publicChild/ShopDetail.vue
@@ -0,0 +1,22 @@
+<template>
+ <div class="box">
+
+ </div>
+</template>
+
+<script>
+export default {
+name:'Box',
+ data() {
+ return {
+
+ }
+ },
+ components: {
+
+ }
+}
+</script>
+
+<style  rel='stylesheet/scss' lang='scss' scoped>
+</style>
